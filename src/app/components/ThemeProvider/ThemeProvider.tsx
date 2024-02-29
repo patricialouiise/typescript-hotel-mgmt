bceb0b0900d20ca03d08c490f5ded4eb5386e668
Patricia Louise Ramos <ramospatricialouise@gmail.com>
2024-02-29 21:39:17 +0800

    create home page and auth screen

---
+++ src/app/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+import ThemeContext from "@/context/themeContext";
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const themeFromLocalStorage: boolean =
+    typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
+      ? JSON.parse(localStorage.getItem("hotel-theme")!)
+      : false;
+
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromLocalStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(true);
+  }, []);
+
+  if (!renderComponent) return <></>;
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
